4c79237a88607558c6b138d4396765b6be2a46a8
ricky-kiva <ricky.kiva@gmail.com>
2023-12-02 18:44:20 +0700

    feat(router): add Get on default endpoint

--- setup_router.go
+++ setup_router.go
@@ -5,6 +5,7 @@ import (
 	"be-go-aot-titan/handler"
 	"be-go-aot-titan/middleware"
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
@@ -24,6 +25,10 @@ func setupRouter(r *gin.Engine, db *gorm.DB, err error) {
 	// require to login to access these
 	r.GET("/aot", middleware.AuthValidation, func(ctx *gin.Context) { handler.GetAllHandler(ctx, db) })
 	r.GET("/aot/:id", middleware.AuthValidation, func(ctx *gin.Context) { handler.GetByIdHandler(ctx, db) })
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": "Dedicate your heart!"})
+	})
 
 	r.PUT("/aot/:id", func(ctx *gin.Context) { handler.PutHandler(ctx, db) })
 
